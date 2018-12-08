1aaaf7194e90f7107a1f9da3b826f910891254b1
Yogev Mizrahi <yogev0mz@gmail.com>
2018-12-08 14:11:05 +0200

    Docker on linux, Swagger support, aspnetcore2.2

--- Invoro.Api/Invoro.Api/Startup.cs
+++ Invoro.Api/Invoro.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Invoro.Api
 {
@@ -22,6 +23,11 @@ namespace Invoro.Api
                     SetPreflightMaxAge(TimeSpan.FromSeconds(2520)));
             });
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
             services.AddMvc();
         }
 
@@ -32,6 +38,16 @@ namespace Invoro.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
