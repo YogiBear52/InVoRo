ab773e93279ffddbb6f3985e6a7a865028097d81
Yogev Mizrahi <yogev0mz@gmail.com>
2019-03-12 19:05:06 +0200

    Client Features Service
    no idea if it works

---
+++ Invoro.WebApp/Invoro/invoro/src/lib/dataModel/Feature.ts
@@ -0,0 +1,6 @@
+export class Feature {
+
+    public id:string;
+    public name:string;
+    public status:string;
+}
---
+++ Invoro.WebApp/Invoro/invoro/src/lib/services/Features.ts
@@ -0,0 +1,17 @@
+import { Feature } from "../dataModel/Feature";
+
+export class FeaturesApi {
+
+    private FEATURES_API:string = 'https://localhost/api/Features';
+
+    public async getFeatures(): Promise<Feature[]> {
+         let response: Response = await fetch(this.FEATURES_API);
+         
+            if (!response.ok) {
+              throw new Error(response.statusText)
+            }
+
+            let responseData : any[] = await response.json();
+            return responseData.map<Feature>(feature => ({id : feature.id, name : feature.name, status : feature.status}));
+        }
+    }
