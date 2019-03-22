539debe21744cb9c4b720f5ebec3514db3ac3516
Lev Levin <levlevin90@gmail.com>
2019-03-22 14:01:28 +0200

    small cleanup and changes

--- Invoro.WebApp/Invoro/invoro/src/lib/dataModel/Feature.ts
+++ Invoro.WebApp/Invoro/invoro/src/lib/dataModel/Feature.ts
@@ -1,6 +1,11 @@
 export class Feature {
-
     public id:string;
     public name:string;
     public status:string;
+
+    public constructor(id: string, name: string, status: string) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
 }
