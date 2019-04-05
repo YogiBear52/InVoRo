6544b369579b4253f6b0f4447677a8f3ea60b9ea
Yogev <miz.yogev@gmail.com>
2019-04-05 17:26:31 +0300

    BetterInnerContainerCommunication

--- Invoro.Api/mongo-seed/import.sh
+++ Invoro.Api/mongo-seed/import.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Delete collection
-mongo mongodb://root:example@10.0.75.1:27017/Invoro?authSource=admin --eval 'db.Features.drop()'
+mongo mongodb://root:example@mongo:27017/Invoro?authSource=admin --eval 'db.Features.drop()'
 
 # Create collection
 # TODO: loop though all collections
-mongoimport --host 10.0.75.1 --port 27017 --username root --password example --authenticationDatabase admin --db Invoro --collection Features --type json --file Features.json --jsonArray
+mongoimport --host mongo --port 27017 --username root --password example --authenticationDatabase admin --db Invoro --collection Features --type json --file Features.json --jsonArray
