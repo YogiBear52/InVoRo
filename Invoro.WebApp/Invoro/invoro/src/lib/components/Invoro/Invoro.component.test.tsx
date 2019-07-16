2b407ac62076dc02acbfef49abeb94e171b5fea1
Yogev <miz.yogev@gmail.com>
2019-07-16 16:14:48 +0300

    Fixed test

--- Invoro.WebApp/Invoro/invoro/src/lib/components/Invoro/Invoro.component.test.tsx
+++ Invoro.WebApp/Invoro/invoro/src/lib/components/Invoro/Invoro.component.test.tsx
@@ -4,6 +4,9 @@ import Invoro from './Invoro.component';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Invoro userIdentifier="FakeUserIdentity" />, div);
+  const fetchMock = (input: RequestInfo, init?: RequestInit) => new Promise<Response>(() => null);
+  ReactDOM.render(<Invoro userIdentifier={"FakeUserIdentity"} fetchApi={fetchMock} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// Check all APIs
